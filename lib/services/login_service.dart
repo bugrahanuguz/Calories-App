aa44bdc7141e00735157081e187fbf25f0b77eb9
bugrahanuguz <burahanuuz@gmail.com>
2023-03-22 17:55:43 +0300

    fix

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -6,7 +6,7 @@ class LoginService {
   String _baseUrl =
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 
-  String apiKey = "AIzaSyCjZEsHcXVg-q679J-2prcl58aLbAtlw3w";
+  String apiKey = "";
   Uri getUrl() => Uri.parse("$_baseUrl$apiKey");
   bool _isLogin = false;
   bool get isLogin => _isLogin;
