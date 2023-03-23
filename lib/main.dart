12c95ea4dc633fd40c48f31f64d8cfd9775bb050
bugrahanuguz <burahanuuz@gmail.com>
2023-03-23 14:11:03 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:softito_final_project/viewmodel/homepage_view_model.dart';
 import 'package:softito_final_project/viewmodel/login_view_model.dart';
 import 'package:softito_final_project/viewmodel/nutritions_view_model.dart';
-
 import 'package:softito_final_project/viewmodel/reset_password.dart';
-
+import 'package:softito_final_project/viewmodel/search_view_model.dart';
 import 'package:softito_final_project/viewmodel/users_view_model.dart';
-import 'package:softito_final_project/views/home_page.dart';
 import 'package:softito_final_project/views/login_page.dart';
-import 'package:softito_final_project/views/search_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,12 +23,8 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => UserViewModel()),
         ChangeNotifierProvider(create: (context) => LoginViewModel()),
         ChangeNotifierProvider(create: (context) => HomepageViewModel()),
-
-        ChangeNotifierProvider(create: (context) => ResetPasswordViewModel())
-
+        ChangeNotifierProvider(create: (context) => ResetPasswordViewModel()),
         ChangeNotifierProvider(create: (context) => SearchViewModel()),
-
-
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
